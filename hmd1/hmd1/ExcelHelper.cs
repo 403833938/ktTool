3a2d6cad262927994f65cdb58364670c6809cdb3
lacey123 <403833938@qq.com>
2018-08-28 17:16:59 +0800

    开通工具

---
+++ hmd1/hmd1/ExcelHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Data.Sql;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace sxst
+{
+    class ExcelHelper
+    {
+        /// <summary>
+        /// 由DataTable导出Excel
+        /// </summary>
+        /// <param name="sourceTable">要导出数据的DataTable</param>
+        /// <returns>Excel工作表</returns>
+        public void ExportDataTableToExcel(DataTable sourceTable, string sheetName, string filepath)
+        {
+
+            FileStream file = new FileStream(filepath, FileMode.Create);
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            // MemoryStream ms = new MemoryStream();
+            ISheet sheet = workbook.CreateSheet(sheetName);
+            IRow headerRow = sheet.CreateRow(0);
+            // handling header.
+            foreach (DataColumn column in sourceTable.Columns)
+
+                headerRow.CreateCell(column.Ordinal).SetCellValue(column.ColumnName);
+            // handling value.
+            int rowIndex = 1;
+            foreach (DataRow row in sourceTable.Rows)
+            {
+                IRow dataRow = sheet.CreateRow(rowIndex);
+                foreach (DataColumn column in sourceTable.Columns)
+                {
+                    dataRow.CreateCell(column.Ordinal).SetCellValue(row[column].ToString());
+                }
+                rowIndex++;
+            }
+            workbook.Write(file);
+            file.Close();
+            sheet = null;
+            headerRow = null;
+            workbook = null;
+        }
+
+    }   
+
+}
---
+++ hmd1/hmd1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hmd1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from 黑名单";
+            DataTable dt = ExecuteReader1(sql).Tables[0];
+            toexcel(dt);
+        }
+        string strcon1 = "server=10.99.8.140;database=剔除;uid=sa;pwd=Dry01!";
+        public System.Data.DataSet ExecuteReader1(string sql)
+        {
+            SqlConnection sqlcon = new SqlConnection(strcon1);
+            try // 正常运行
+            {
+                sqlcon.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, sqlcon);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                sqlcon.Close();
+                return ds;
+            }
+            catch (OleDbException ole) // 数据库操作异常处理
+            {
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    // 关闭数据库连接
+                    sqlcon.Close();
+                }
+                // 返回失败
+                //LogBLL.debug(ole.ToString());
+                return null;
+            }
+            catch (Exception ee)// 异常处理
+            {
+
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    // 关闭数据库连接
+
+                    sqlcon.Close();
+                }
+                // 返回失败
+                //LogBLL.debug(ee.ToString());
+                return null;
+            }
+        finally // 执行完毕清除在try块中分配的任何资源
+            {
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    // 关闭数据库连接
+                    sqlcon.Close();
+                }
+            }
+        }
+
+
+        private void toexcel(DataTable dt)
+        {
+            if (dt != null)
+            {
+                string localFilePath, fileNameExt, FilePath;
+                SaveFileDialog sfd = new SaveFileDialog();
+
+                //设置文件类型 
+                sfd.Filter = "Excel文件(*.xls)|*.xls|Excel文件(*.xlsx)|*.xlsx";
+                //name = Path.GetFileName(nametxt);
+                //默认文件名
+                sfd.FileName = DateTime.Now.ToString("yyyyMMddHHmmss") + "_反馈" + ".xls";
+                //保存对话框是否记忆上次打开的目录 
+                //sfd.RestoreDirectory = true;
+
+                //点了保存按钮进入 
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    localFilePath = sfd.FileName.ToString(); //获得文件路径 
+                    fileNameExt = localFilePath.Substring(localFilePath.LastIndexOf("\\") + 1); //获取文件名，不带路径
+
+                    //获取文件路径，不带文件名 
+                    FilePath = localFilePath.Substring(0, localFilePath.LastIndexOf("\\"));
+                    sxst.ExcelHelper ex = new sxst.ExcelHelper();
+
+                    ex.ExportDataTableToExcel(dt, fileNameExt, localFilePath);
+
+                }
+            }
+
+        }
+    }
+}
